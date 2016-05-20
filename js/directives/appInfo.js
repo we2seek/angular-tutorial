cc42a614fcd5e19f5494e0b7df29beee892b1319
Vitaliy Timchenko [home] <vitaliy.timchenko@gmail.com>
2016-05-20 10:27:56 +0300

    Initial commit with some created directives

---
+++ js/directives/appInfo.js
@@ -0,0 +1,12 @@
+'use strict'
+
+app.directive('appInfo', function(){
+    return {
+        restrict: 'E',
+        scope: {
+            info: '='
+        },
+        templateUrl: 'js/directives/appInfo.html',
+
+    };
+});
