bb0eb3d7f23f59a8c33040f8ba7188cc43d529bb
Vitaliy Timchenko [home] <vitaliy.timchenko@gmail.com>
2016-05-23 20:48:25 +0300

    Pretty URL: remove hash(#) from URL

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 var app = angular.module('GalleryApp', ['ngRoute']);
 
-app.config(function($routeProvider){
-  $routeProvider
+app.config(function($routeProvider, $locationProvider){
+
+    $routeProvider
     .when('/',{
-    controller: 'HomeController',
-    templateUrl: 'views/home.html'
-  })
+        controller: 'HomeController',
+        templateUrl: 'views/home.html'
+    })
     .when('/photos/:id', {
-    controller: 'PhotoController',
-    templateUrl: 'views/photo.html'
-  })
+        controller: 'PhotoController',
+        templateUrl: 'views/photo.html'
+    })
     .otherwise({
-    redirectTo: '/'
-  });
+        redirectTo: '/'
+    });
+
+   // use the HTML5 History API
+   $locationProvider.html5Mode(true);
 });
